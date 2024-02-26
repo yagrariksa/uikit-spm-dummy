f5cba8c4741cf1acd11182febd7183932157fae1
yagrariksa <luckyrama0@gmail.com>
2024-02-26 17:13:15 +0700

    add Helper to test Bundle
    Signed-off-by: yagrariksa <luckyrama0@gmail.com>

---
+++ a/Module/Classes/Helper.swift
@@ -0,0 +1,14 @@
+//
+//  Helper.swift
+//  a
+//
+//  Created by Daffa Yagrariksa on 26/02/24.
+//
+
+import Foundation
+
+class Helper {
+    static var bundle: Bundle {
+        return Bundle(for: TestViewController.self)
+    }
+}
