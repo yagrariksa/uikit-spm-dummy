70d5e9c0d2111c3ad89b5665bb83bcde5e78e27c
yagrariksa <luckyrama0@gmail.com>
2024-02-26 11:32:31 +0700

    create navigation
    Signed-off-by: yagrariksa <luckyrama0@gmail.com>

--- a/ViewController.swift
+++ a/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var button: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func navToTestView(_ sender: UIButton) {
+        
+        performSegue(withIdentifier: "showTestView", sender: sender)
+        print("Triggered")
+        
+    }
 }
 
