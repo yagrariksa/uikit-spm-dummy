62400b03419e01b58cb844966e5c7ff7d0df7f96
yagrariksa <luckyrama0@gmail.com>
2024-02-27 11:39:46 +0700

    remove Bundle+Extension
    Signed-off-by: yagrariksa <luckyrama0@gmail.com>

--- a/Module/Classes/Bundle+Extension.swift
+++ a/Module/Classes/Bundle+Extension.swift
@@ -7,12 +7,3 @@
 
 import Foundation
 
-extension Bundle {
-    public static var aModule: Bundle {
-        #if SWIFT_PACKAGE
-        return Bundle.module
-        #else
-        return Bundle(for: TestViewController.self)
-        #endif
-    }
-}
