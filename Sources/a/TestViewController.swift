a42b16a67bc4554c38fa9d86dea523951de1cd75
yagrariksa <luckyrama0@gmail.com>
2024-03-14 12:47:57 +0700

    try variant programaticly layouting tableviewCell
    Signed-off-by: yagrariksa <luckyrama0@gmail.com>

--- Sources/a/TestViewController.swift
+++ Sources/a/TestViewController.swift
@@ -26,7 +26,7 @@ public class TestViewController: UIViewController {
     public override func viewDidLoad() {
 //        let customCellNib = UINib(nibName: "CustomCell", bundle: Bundle.module)
 //        tableView.register(customCellNib, forCellReuseIdentifier: "customCell")
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "custom2")
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "custom3")
     }
 }
 
@@ -36,8 +36,25 @@ extension TestViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "custom2", for: indexPath) as! CustomCell2
-        cell.set(CustomCellData(textLabel: "Label \(indexPath.row + 1)"))
+        let cell = tableView.dequeueReusableCell(withIdentifier: "custom3", for: indexPath)
+        // cell.set(CustomCellData(textLabel: "Label \(indexPath.row + 1)"))
+        let image = UIImage(systemName: "square.and.arrow.down.fill")
+        let imageView = UIImageView(image: image)
+        cell.addSubview(imageView)
+        
+        let label = UILabel()
+        label.text = "custom3 \(indexPath.row + 1)"
+        cell.addSubview(label)
+        
+        let imageCenterYConstraint = NSLayoutConstraint(item: imageView, attribute: .centerY, relatedBy: .equal, toItem: cell, attribute: .centerX, multiplier: 1, constant: 0)
+        let imageLeadingConstraint = NSLayoutConstraint(item: imageView, attribute: .leading, relatedBy: .equal, toItem: cell, attribute: .leading, multiplier: 1, constant: 16)
+        
+        let labelCenterYConstraint = NSLayoutConstraint(item: label, attribute: .centerY, relatedBy: .equal, toItem: cell, attribute: .centerY, multiplier: 1, constant: 0)
+        let labelLeadingConstraint = NSLayoutConstraint(item: label, attribute: .leading, relatedBy: .equal, toItem: imageView, attribute: .trailing, multiplier: 1, constant: 16)
+        let labelTrailingConstraint = NSLayoutConstraint(item: label, attribute: .trailing, relatedBy: .greaterThanOrEqual, toItem: cell, attribute: .trailing, multiplier: 1, constant: 16)
+        
+        NSLayoutConstraint.activate([imageCenterYConstraint, imageLeadingConstraint, labelCenterYConstraint, labelLeadingConstraint, labelTrailingConstraint])
+        
         return cell
     }
     
